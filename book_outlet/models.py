7e46ac22251cb1495d6304ceed1c5ade0b301784
unknown <xxx@gmail.com>
2021-07-30 18:32:34 +0800

    final cut

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,6 +2,7 @@ from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -9,10 +10,15 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True) #Harry Potter 1 => harry-potter-1
 
     def get_absolute_url(self):
-        return reverse("book_detail", args=[self.id])
-    
+        return reverse("book_detail", args=[self.slug])
+
+    def save(self, *arg, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*arg, **kwargs)
+
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
