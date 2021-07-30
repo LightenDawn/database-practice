1bb7cc52b3c8d8f09422aa81e52228bd8a72d646
unknown <xxx@gmail.com>
2021-07-30 16:47:18 +0800

    finished the basic setting of the html and wiring up

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
@@ -8,6 +9,10 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.id])
+    
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
